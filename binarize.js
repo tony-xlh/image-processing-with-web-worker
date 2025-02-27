734ce01a2410ce6c01fd708f9946e197dee60013
tony-xlh <tony@dynamsoft.com>
2025-02-27 13:16:20 +0800

    add otsu

--- binarize.js
+++ binarize.js
@@ -12,6 +12,12 @@ function binarize(imageData,threshold){
     const grayscale = convertToGrayscale(red, green, blue);
     grayscaleValues.push(grayscale);
   }
+
+  if (threshold === -1) {
+    threshold = ostu(grayscaleValues);
+  }
+  
+  console.log("threshold", threshold);
   let grayscaleIndex = 0;
   for (let i = 0; i < pixels.length; i += 4) {
     const gray = grayscaleValues[grayscaleIndex];
@@ -33,4 +39,74 @@ onmessage = (e) => {
   binarize(data.imageData, data.threshold);
   console.log("Posting message back to main script");
   postMessage({imageData:data.imageData, index:data.index});
+};
+
+const histo = (data, bins) =>
+  data.reduce((arr, e) => {
+    arr[bins.indexOf(e)] += 1;
+    return arr;
+  }, [...Array(bins.length)].fill(0));
+
+const width = (hist, s, e) => {
+  let v = 0;
+  for (let i = s; i < e; i += 1) {
+    v += hist[i];
+  }
+  return v;
+};
+
+const bins = data => Array.from(new Set(data)).sort((e0, e1) => e0 - e1);
+
+const weight = (hist, s, e, total) => {
+  let v = 0;
+  for (let i = s; i < e; i += 1) {
+    v += hist[i];
+  }
+  return v / total;
+};
+
+const mean = (hist, bins, s, e, width) => {
+  let v = 0;
+  for (let i = s; i < e; i += 1) {
+    v += hist[i] * bins[i];
+  }
+  return v * width;
+};
+
+const variance = (hist, bins, s, e, mean, width) => {
+  let v = 0;
+  for (let i = s; i < e; i += 1) {
+    const d = bins[i] - mean;
+    v += d * d * hist[i];
+  }
+  return v * width;
+};
+
+const cross = (wb, vb, wf, vf) => wb * vb + wf * vf;
+
+function ostu(data) {
+  const b = bins(data);
+  const h = histo(data, b);
+  const { length: total } = data;
+  const vars = [...Array(b.length)].map((_, i) => {
+    const s0 = 0;
+    const e0 = i;
+    const s1 = i;
+    const e1 = h.length;
+
+    const w0 = 1 / width(h, s0, e0);
+    const w1 = 1 / width(h, s1, e1);
+
+    const wb = weight(h, s0, e0, total);
+    const vb = variance(h, b, s0, e0, mean(h, b, s0, e0, w0), w0);
+
+    const wf = weight(h, s1, e1, total);
+    const vf = variance(h, b, s1, e1, mean(h, b, s1, e1, w1), w1);
+
+    const x = cross(wb, vb, wf, vf);
+
+    return !isNaN(x) ? x : Number.POSITIVE_INFINITY;
+  });
+
+  return b[vars.indexOf(Math.min(...vars))];
 };
