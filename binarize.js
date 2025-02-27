d3eaf628f77127fd68413108cfee7e517a432cad
tony-xlh <tony@dynamsoft.com>
2025-02-27 10:59:21 +0800

    basic implementation

---
+++ binarize.js
@@ -0,0 +1,36 @@
+function convertToGrayscale(r, g, b) {
+  return (r * 6966 + g * 23436 + b * 2366) >> 15;
+}
+
+function process(imageData,threshold){
+  const pixels = imageData.data; //[r,g,b,a,...]
+  const grayscaleValues = [];
+  for (let i = 0; i < pixels.length; i += 4) {
+    const red = pixels[i];
+    const green = pixels[i + 1];
+    const blue = pixels[i + 2];
+    const grayscale = convertToGrayscale(red, green, blue);
+    grayscaleValues.push(grayscale);
+  }
+  let grayscaleIndex = 0;
+  for (let i = 0; i < pixels.length; i += 4) {
+    const gray = grayscaleValues[grayscaleIndex];
+    grayscaleIndex = grayscaleIndex + 1;
+    let value = 255;
+    if (gray < threshold) {
+      value = 0;
+    }
+    pixels[i] = value;
+    pixels[i + 1] = value;
+    pixels[i + 2] = value;
+  }
+}
+
+onmessage = (e) => {
+  console.log(e);
+  console.log("Message received from main script");
+  let data = e.data;
+  process(data.imageData, data.threshold);
+  console.log("Posting message back to main script");
+  postMessage({imageData:data.imageData});
+};
